70a222bcdd416ba578bc8efb2d90f4559a848cb2
Nis Sarup <nis@sarup.dk>
2010-05-06 10:55:16 +0200

    Added interrupt-test & lcd defenitions-file.

--- display_test/interrupt_test.asm
+++ display_test/interrupt_test.asm
@@ -1,38 +1,62 @@
 .include "../includes/m32def.inc"
 .include "../includes/lcdDef.inc"	; Display definitions.
 
+.org 0x00
+	jmp Main
+.org 0x02
+	jmp Reset
+	
+	
+.org 0x100
 
-		ldi r16,high(RAMEND)	; sæt stakken op
-		out sph,r16
-		ldi r16,low(RAMEND)
-		out spl,r16
+Reset:
+	ldi r18,0x01		; clear lcd
+	call cmdwrt
+	call delay_2ms
+	ldi r18,0x06		; shift cursor right
+	call cmdwrt
+	LDI R31,HIGH(MSG_2<<1)
+	LDI R30,LOW(MSG_2<<1)		; Z points to HELLO_WORLD
+	rjmp loop
 
-		ldi r16,0xFF
-		out lcd_dddr,r16	; command er output
-		out lcd_cddr,r16	; data er output
+Main:
+	ldi r16,high(RAMEND)	; sæt stakken op
+	out sph,r16
+	ldi r16,low(RAMEND)
+	out spl,r16
+	
+	SBI PORTD,2		; Init INT0-knap
+	LDI R16,1<<INT0
+	OUT GICR,R16
 
-		cbi lcd_cprt,lcd_en	; enable = 0
-		call delay_2ms
-		ldi r18,0x38		; lcd 5x7 matrix 2 linier
-		call cmdwrt
-		call delay_2ms
-		ldi r18,0x0E		; display tændt, cursor on
-		call cmdwrt
-		ldi r18,0x01		; clear lcd
-		call cmdwrt
-		call delay_2ms
-		ldi r18,0x06		; shift cursor right
-		call cmdwrt
-		
-		LDI R31,HIGH(NIS_STYRER<<1)
-		LDI R30,LOW(NIS_STYRER<<1)		; Z points to HELLO_WORLD
-		
+	ldi r16,0xFF
+	out lcd_dddr,r16	; command er output
+	out lcd_cddr,r16	; data er output
+
+	cbi lcd_cprt,lcd_en	; enable = 0
+	call delay_2ms
+	ldi r18,0x38		; lcd 5x7 matrix 2 linier
+	call cmdwrt
+	call delay_2ms
+	ldi r18,0x0E		; display tændt, cursor on
+	call cmdwrt
+	ldi r18,0x01		; clear lcd
+	call cmdwrt
+	call delay_2ms
+	ldi r18,0x06		; shift cursor right
+	call cmdwrt
+	
+	LDI R31,HIGH(MSG_1<<1)
+	LDI R30,LOW(MSG_1<<1)		; Z points to HELLO_WORLD
+	
+	SEI
+	
 LOOP:	LPM R18,Z+
 		CPI R18,0		; Compare R16 with 0
 		BREQ here		; Exit if 0
 		CALL datawrt	; Write data
 		rjmp LOOP
-		
+
 here:	jmp here
 
 .include "../includes/strings.inc"
